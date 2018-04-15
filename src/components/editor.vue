5ee92703d94f77806362fb5f4706eb3a1ea365ec
cuijinyu <2510271615@qq.com>
2018-04-15 14:26:11 +0800

    增加了操作工具栏

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="main">
+    <div class="operator">
+      <a href="" v-on:click.prevent="addTitle"><img src="../assets/images/文案标题.png" alt="一级标题" title="添加一级标题"></a>
+      <a href="" v-on:click.prevent="addBold"><img src="../assets/images/粗体.png" alt="粗体" title="添加粗体" ></a>
+      <a href="" v-on:click.prevent="addItalic"><img src="../assets/images/斜体.png" alt="斜体" title="添加斜体"></a>
+      <a href="" v-on:click.prevent="addList"><img src="../assets/images/列表.png" alt="列表" title="添加列表"></a>
+      <a href="" v-on:click.prevent="addQuote"><img src="../assets/images/引用.png" alt="引用" title="添加引用"></a>
+      <a href="" v-on:click.prevent="addLink"><img src="../assets/images/链接.png" alt="链接" title="添加链接"></a>
+      <a href="" v-on:click.prevent="addCode"><img src="../assets/images/符号-代码.png" alt="代码" title="添加代码"></a>
+      <a href="" v-on:click.prevent="addImage"><img src="../assets/images/图片.png" alt="图片" title="添加图片"></a>
+      <a href="" v-on:click.prevent="addTable"><img src="../assets/images/表格.png" alt="表格" title="添加表格"></a>
+      <a href="" v-on:click.prevent="addMath"><img src="../assets/images/数学.png" alt="数学公式" title="添加数学公式"></a>
+      <a href="" v-on:click.prevent="addBreaker"><img src="../assets/images/758编辑器_分割线.png" alt="分割线" title="添加分割线"></a>
+    </div>
     <div class="editor">
       <textarea name="" id="input-textarea" cols="30" rows="30" v-model="input"  wrap="hard" v-on:keyup.9="keydown">
       </textarea>
@@ -38,9 +51,89 @@ let editor = {
      */
     keydown(){
       let pos = this.op.getPosition().start;
+      debugger;
       this.op.addTab();
       this.input = document.getElementById("input-textarea").value;
       this.op.setPosition(pos+4,pos+4);
+      this.output = Parser.parse(this.input);
+    },
+    addTitle(){
+      let pos = this.op.getPosition().start;
+      debugger;
+      console.log(this.op);
+      this.op.addTitle();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+1,pos+1);
+      this.output = Parser.parse(this.input);
+    },
+    addCode(){
+      let pos = this.op.getPosition().start;
+      this.op.addCode();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+4,pos+4);
+      this.output = Parser.parse(this.input);
+    },
+    addBold(){
+      let pos = this.op.getPosition().start;
+      this.op.addBold();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
+      this.output = Parser.parse(this.input);
+    },
+    addQuote(){
+      let pos = this.op.getPosition().start;
+      this.op.addQuote();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
+      this.output = Parser.parse(this.input);
+    },
+    addTable(){
+      let pos = this.op.getPosition().start;
+      this.op.addTable();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+87,pos+87);
+      this.output = Parser.parse(this.input);
+    },
+    addList(){
+      let pos = this.op.getPosition().start;
+      this.op.addList();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
+      this.output = Parser.parse(this.input);
+    },
+    addBreaker(){
+      let pos = this.op.getPosition().start;
+      this.op.addBreaker();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+13,pos+13);
+      this.output = Parser.parse(this.input);
+    },
+    addItalic(){
+      let pos = this.op.getPosition().start;
+      this.op.addItalic();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
+      this.output = Parser.parse(this.input);
+    },
+    addLink(){
+      let pos = this.op.getPosition().start;
+      this.op.addLink();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
+      this.output = Parser.parse(this.input);
+    },
+    addImage(){
+      let pos = this.op.getPosition().start;
+      this.op.addPics();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
+      this.output = Parser.parse(this.input);
+    },
+    addMath(){
+      let pos = this.op.getPosition().start;
+      this.op.addLatex();
+      this.input = document.getElementById("input-textarea").value;
+      this.op.setPosition(pos+2,pos+2);
       this.output = Parser.parse(this.input);
     }
   },
@@ -48,9 +141,9 @@ let editor = {
     input:function () {
       let self = this;
       //初始化对文本编辑器的操作
-      if(this.op == ''){
-        this.op = new TextOperation(document.getElementById("input-textarea"));
-      }
+      // if(this.op == ''){
+      //   this.op = new TextOperation(document.getElementById("input-textarea"));
+      // }
       /**
        * 节流器
        */
@@ -72,6 +165,11 @@ let editor = {
           },2000);
         }
     }
+  },
+  mounted:function(){
+    if(this.op == ''){
+      this.op = new TextOperation(document.getElementById("input-textarea"));
+    }
   }
 }
 
@@ -80,6 +178,9 @@ export default editor;
 </script>
 
 <style scoped lang="less">
+  body{
+    margin:0px;
+  }
   *{
     font-size:14px;
     margin:0;
@@ -87,6 +188,43 @@ export default editor;
   }
   br{
     line-height: 5px;
+  }
+  .operator{
+    position:relative;
+    height:30px;
+    width:396px;
+    box-shadow:2px 2px 2px rgb(249,249,249);
+    margin-bottom:5px;
+    background:rgb(249,249,249);
+    border:1px solid rgba(0,0,0,0.2);
+    border-radius:5px;
+  }
+  .operator img{
+    position:relative;
+    top:5px;
+    width:20px;
+    height:20px;
+  }
+  .operator a{
+    float:left;
+    border-right:1px solid rgba(0,0,0,0.3);
+    border-radius:5px;
+    width:35px;
+    height:30px;
+    /*margin-right: 5px;*/
+    text-align:center;
+  }
+  .operator a:last-child{
+    float:left;
+    border-right:0px solid rgba(0,0,0,0.3);
+    /*border-radius:5px;*/
+    width:35px;
+    height:30px;
+    /*margin-right: 5px;*/
+    text-align:center;
+  }
+  .operator a:hover{
+    background-color: rgb(255,245,212);
   }
   .main{
     width:100%;
